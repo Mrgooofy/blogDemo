02bdb62afbae80a97cf3e42d51bc02934f7e0673
kc <codelittle@outlook.com>
2020-09-13 01:18:11 +0800

    改变配置的格式， 使用jjwt的验证 token

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ blog demo learn
 使用 Lombok 插件
 使用 swagger 文档插件
  
+配置文件 改为 applicaiton.yml 格式
--- src/main/java/com/gooofy/demo/DemoApplication.java
+++ src/main/java/com/gooofy/demo/DemoApplication.java
@@ -1,11 +1,20 @@
 package com.gooofy.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+
 @EnableSwagger2
+// @ComponentScan(basePackages={"com.gooofy.demo.controller","com.gooofy.demo.service"})
+@Component
+@SpringBootApplication(scanBasePackages = {"com.gooofy.demo.*"})
+@MapperScan(basePackages ={"com.gooofy.demo.mapper"})
+@ServletComponentScan(basePackages = "com.gooofy.demo.security")
 public class DemoApplication {
 
     public static void main(String[] args) {
